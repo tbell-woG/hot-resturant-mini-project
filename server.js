0443a446658f9e4333797cd6fd8d5c432c304dd5
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-05 02:47:32 -0500

    Starter files & some js & HTML

---
+++ server.js
@@ -0,0 +1,27 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+ 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+});
